d02eef5e1e89d22642dcc360623c087146cea045
nicolamarchiotto <ziapo98@gmail.com>
2022-07-12 17:32:28 +0200

    retrieve robot pose and orientation, tried to change line position at run time in Unity but I did not succed, changed directory structure to allow build of colcon workspace

--- Turtlebot3UnityROS2/Assets/Scripts/lineSpawner.cs
+++ Turtlebot3UnityROS2/Assets/Scripts/lineSpawner.cs
@@ -10,13 +10,19 @@ public class lineSpawner : MonoBehaviour
     {
         GameObject line = Instantiate(linePrefab) as GameObject;
         line.transform.parent = gameObject.transform;
-        line.transform.localPosition = new Vector3(0.0f,0.0f,0.0f);
+        line.transform.localPosition = new Vector3(0.0f,0.0f,0.35f);
         // line.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    	//Destroy(linePrefab)
+    	//playerPosition.y += 1;
+      	//GameObject newFurbot = Instantiate(furbot, playerPosition, Quaternion.identity);
+      	//Destroy(furbot);
+      	//furbot = newFurbot;
+       //GameObject clone = (GameObject)Instantiate(linePrefab,transform.position+new Vector3(0.0f,0.0f,0.01f),Quaternion.identity);
+       //Destroy (clone, 1.0f);
     }
 }
